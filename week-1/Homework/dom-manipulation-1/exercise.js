9bb665d30873cc8b0769837565bdd4d8c5139539
mattndross <mattndross@users.noreply.github.com>
2021-07-31 00:48:58 +0200

    exercise complete

--- week-1/Homework/dom-manipulation-1/exercise.js
+++ week-1/Homework/dom-manipulation-1/exercise.js
@@ -68,3 +68,104 @@ Using the same function in Task 3, every time the 'Change colour' button is clic
 The next color when you are in the last color of the array will be the first color again.
 */
 
+
+/* TASK 1 */
+const allTheP = document.querySelectorAll('p');
+console.log(allTheP);
+
+const firstDiv = document.querySelector('div');
+console.log(firstDiv);
+
+const jumbotron = document.querySelector('#jumbotron-text');
+console.log(jumbotron);
+
+const allThePInPrimary = document.querySelectorAll('.primary-content p');
+console.log(allThePInPrimary);
+
+/* TASK 2 */
+document.querySelector('#alertBtn').addEventListener('click', alertThanks);
+
+function alertThanks() {
+    alert("Thanks for visiting Bikes for Refugees!");
+}
+
+/* TASK 3 */
+/*  EDITED IN TASK 7
+document.querySelector('#bgrChangeBtn').addEventListener('click', changeBgrColour);
+const body = document.querySelector('body');
+
+function changeBgrColour(){
+    if (body.className == 'bg-primary') {
+        body.classList.remove('bg-primary');
+    } else {
+        body.classList.add('bg-primary');
+    }
+}
+*/
+
+/* TASK 4 */
+const addTextBtn = document.querySelector('#addTextBtn');
+const mainArticles = document.querySelector('#mainArticles');
+addTextBtn.addEventListener('click', addP);
+
+function addP() {
+    const paragraph = document.createElement('p');
+    mainArticles.appendChild(paragraph);
+}
+
+/* TASK 5 */
+const allLinks = [...document.querySelectorAll('a')];
+const largerLinksBtn = document.querySelector('#largerLinksBtn');
+largerLinksBtn.addEventListener('click', increaseLinksFontSize);
+console.log(allLinks)
+
+function increaseLinksFontSize() {
+    allLinks.map(link => link.style.fontSize = '1.3em');
+}
+
+/* TASK 6 */
+const addArticleBtn = document.querySelector('#addArticleBtn');
+addArticleBtn.addEventListener('click', addArticle);
+
+function addArticle() {
+    //create article element and add class
+    const article = document.createElement('article');
+    article.classList.add('article');
+
+
+    //create p element and add class
+    const paragraph = document.createElement('p');
+    paragraph.classList.add('article-lead')
+    
+    // append childs to each parents
+    article.appendChild(paragraph);
+    mainArticles.appendChild(article);
+    
+    //get the text inside the input field
+    const inputText = document.querySelector('.addArticle').value;
+    paragraph.textContent = inputText.trim();
+    
+    //clear the input field
+    document.querySelector('.addArticle').value = '';
+}
+
+/* TASK 7 */
+
+//Create an array of 5 different colors
+const fiveColours = ['bg-primary', 'bg-danger', 'bg-success', 'bg-warning', 'bg-white']
+
+//add an event when the 'change colour' button is clicked
+document.querySelector('#bgrChangeBtn').addEventListener('click', changeBgrColour);
+const body = document.querySelector('body');
+
+function changeBgrColour(){
+    let colourIndex = fiveColours.indexOf(body.className);
+    if (body.className === fiveColours[4]) {
+        body.classList.remove(fiveColours[4])
+        body.classList.add(fiveColours[0]);
+    } else {
+        body.classList.remove(fiveColours[colourIndex])
+        body.classList.add(fiveColours[colourIndex + 1]);
+    }
+    console.log(body.classList)
+}
