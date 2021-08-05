8eeada7307f19f16c2309d3384bf7f4c61a47ceb
mattndross <mattndross@users.noreply.github.com>
2021-08-05 21:23:34 +0200

    part 1 done

--- week-2/Homework/Part-1/exercises.js
+++ week-2/Homework/Part-1/exercises.js
@@ -13,8 +13,20 @@
  *      .....
  * </div>
  */
+let content = document.querySelector("#content");
+
 function exerciseOne(arrayOfPeople) {
-  let content = document.querySelector("#content");
+  arrayOfPeople.forEach(person => {
+    const nameOfPerson = document.createElement('h1');
+    const jobOfPerson = document.createElement('h2');
+
+    nameOfPerson.textContent = person.name;
+    jobOfPerson.textContent = person.job;
+
+    content.appendChild(nameOfPerson);
+    content.appendChild(jobOfPerson);
+    
+  });
 }
 
 /**
@@ -25,6 +37,18 @@ function exerciseOne(arrayOfPeople) {
  *
  */
 function exerciseTwo(shopping) {
+  let list = document.createElement('ul');
+  content.appendChild(list);
+  list.textContent = 'Shopping List';
+
+  shopping.forEach(item => {
+    const shoppingItem = document.createElement('li');
+    shoppingItem.textContent = item;
+    list.appendChild(shoppingItem);
+
+  })
+
+
   //Write your code in here
 }
 
@@ -58,7 +82,41 @@ function exerciseTwo(shopping) {
     The end result should look something like this: https://hyf-js2-week1-makeme-ex1-demo.herokuapp.com/
 **/
 function exerciseThree(books) {
-  //Write your code in here
+  const listOfBooks = document.createElement('ul');
+  content.appendChild(listOfBooks);
+
+  books.forEach(book => {
+    const bookCard = document.createElement('li');
+    listOfBooks.appendChild(bookCard)
+
+    //add p element
+    const titleAndAuthor = document.createElement('p');
+    titleAndAuthor.textContent = `${book.title} - ${book.author}`;
+    bookCard.appendChild(titleAndAuthor);
+    
+    //add image
+    const bookImg = document.createElement('img');
+    bookCard.appendChild(bookImg);
+
+    if (book.title === "The Design of Everyday Things") {
+      bookImg.src = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.mbhNiA2Q2BfwlertZB0VSwHaHa%26pid%3DApi&f=1';
+    } 
+    else if (book.title === "The Most Human Human"){
+      bookImg.src = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.ACZUFam6BxRcIc8PcTkGhwHaK_%26pid%3DApi&f=1'
+    }
+    else if (book.title === "The Pragmatic Programmer") {
+      bookImg.src = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.KI_tsGHe4sBa0xd3WjpMyQHaJs%26pid%3DApi&f=1'
+    }
+
+    //color 
+
+    if (book.alreadyRead === true) {
+      bookCard.style.backgroundColor = 'green';
+    } else {
+      bookCard.style.backgroundColor = 'red';
+    }
+
+  })
 }
 
 //
