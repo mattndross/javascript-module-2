467aa66610d2e62cbdcdd97945df7c00afcdf0b0
mattndross <matiasandresrossi@gmail.com>
2021-09-26 11:58:08 -0300

    exercise c-alarmclock done

--- week-3/Homework/C-alarmclock/alarmclock.js
+++ week-3/Homework/C-alarmclock/alarmclock.js
@@ -1,11 +1,43 @@
-const setButton = document.querySelector('#set')
-setButton.addEventListener('click', () =>{
-  const alarmSet = document.querySelector('#alarmSet').value;
-  document.querySelector('#timeRemining').value = alarmSet;
-})
+let count = 0;
+
+
 function setAlarm() {
+  const title = document.querySelector("#timeRemaining");
+}
+
+function startTimer(duration, display) {
+  let timer = duration,
+    minutes, seconds;
+  const interval = setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = `Time Remaining: ${minutes}:${seconds}`;
+
+    if (--timer < 0) {
+      timer = 0;
+      clearInterval(interval);
+      playAlarm();
+    }
+
+    console.log('timer', timer)
+  }, 1000);
+
+  
 
 }
+
+document.querySelector("#set").addEventListener("click", (e) => {
+  e.preventDefault();
+  const userInput = parseInt(document.querySelector("#alarmSet").value);
+  count = userInput;
+  const displayCount = document.querySelector("#timeRemaining");
+  startTimer(count, displayCount);
+})
+
 
 // DO NOT EDIT BELOW HERE
 
